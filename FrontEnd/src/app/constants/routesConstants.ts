20b2f63ab09ee1e265926e436c7892884e081a85
Manav Gohel <2303manavgohel@gmail.com>
2026-05-03 18:03:39 +0530

    added constant files

---
+++ FrontEnd/src/app/constants/routesConstants.ts
@@ -0,0 +1,9 @@
+export const EMPTY_STRING: string = "";
+export const ROOT_PATH: string = "/";
+export const RENT_PROPERTY: string = "rent-property";
+export const ADD_PROPERTY: string = "add-property";
+export const DETAILS_PROPERTY_ID: string = "property-details/:id";
+export const DETAILS_PROPERTY: string = "property-details";
+
+export type TSellRent = "Sell" | "Rent";
+export type TPropertiesImage = "prop-1" | "prop-2" | "prop-3" | "prop-4";
