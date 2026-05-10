c79942ebfd8fc8b521648e1cf9a9227be487debf
Manav Gohel <2303manavgohel@gmail.com>
2026-05-10 18:14:45 +0530

    Revert "dropdown for logout added"
    This reverts commit 0eddcb72bf94ebd9113f832abec05f9d55a0756b.

--- FrontEnd/src/app/nav-bar/nav-bar.ts
+++ FrontEnd/src/app/nav-bar/nav-bar.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Router, RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
 import {
   ADD_PROPERTY,
   RENT_PROPERTY,
@@ -7,12 +7,10 @@ import {
   USER_LOGIN,
   USER_REGISTER,
 } from "../constants/routesConstants";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
 @Component({
   selector: "app-nav-bar",
-  imports: [RouterLink, RouterLinkActive, RouterModule, BrowserAnimationsModule, BsDropdownModule],
+  imports: [RouterLink, RouterLinkActive, RouterModule],
   templateUrl: "./nav-bar.html",
   styleUrl: "./nav-bar.scss",
   standalone: true,
@@ -25,15 +23,4 @@ export class NavBar {
   ADD_PROPERTY = ADD_PROPERTY;
   USER_REGISTER = USER_REGISTER;
   USER_LOGIN = USER_LOGIN;
-
-  constructor(private readonly router: Router) {}
-
-  get isLoggedIn() {
-    return localStorage.getItem("token");
-  }
-
-  onLogout() {
-    localStorage.removeItem("token");
-    this.router.navigate([USER_LOGIN]);
-  }
 }
