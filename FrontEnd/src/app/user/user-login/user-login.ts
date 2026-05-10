c384956d76cb0c1e60ccae73e006a533f857490f
Manav Gohel <2303manavgohel@gmail.com>
2026-05-10 15:13:05 +0530

    added login form with template driven approach

--- FrontEnd/src/app/user/user-login/user-login.ts
+++ FrontEnd/src/app/user/user-login/user-login.ts
@@ -1,9 +1,16 @@
 import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-user-login",
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./user-login.html",
   styleUrl: "./user-login.scss",
 })
-export class UserLogin {}
+export class UserLogin {
+  constructor() {}
+
+  onLogin(loginForm: NgForm) {
+    console.log(loginForm.value);
+  }
+}
